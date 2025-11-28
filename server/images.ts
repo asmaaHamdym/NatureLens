fb35646ce4f092659cef37ed8cc607e76e9852f5
asmaaHamdym <asmahamdym@gmail.com>
2025-11-28 07:23:39 +0200

    add ai boilerplate code

--- server/images.ts
+++ server/images.ts
@@ -1,28 +1,26 @@
 "use server";
-import { google } from "@ai-sdk/google";
 import { generateText } from "ai";
 import fs from "node:fs";
 import "dotenv/config";
 
-async function generateImage() {
+export async function generateImage(prompt: string) {
   const result = await generateText({
-    model: google("gemini-2.5-flash-image-preview"),
-    prompt:
-      "Create a picture of a nano banana dish in a fancy restaurant with a Gemini theme",
+    model: "gemini-2.5-flash-image-preview",
+    prompt,
   });
+
+  let fileName;
 
   // Save generated images
   for (const file of result.files) {
     if (file.mediaType.startsWith("image/")) {
       const timestamp = Date.now();
-      const fileName = `generated-${timestamp}.png`;
+      fileName = `generated-${timestamp}.png`;
 
-      fs.mkdirSync("output", { recursive: true });
-      await fs.promises.writeFile(`output/${fileName}`, file.uint8Array);
+      await fs.promises.writeFile(`puplic/${fileName}`, file.uint8Array);
 
-      console.log(`Generated and saved image: output/${fileName}`);
+      console.log(`Generated and saved image: puplic/${fileName}`);
     }
   }
+  return `/${fileName}`;
 }
-
-generateImage().catch(console.error);
