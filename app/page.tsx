dbfde19bf33fa6ef16651c35316bd3f310a62cba
asmaaHamdym <asmahamdym@gmail.com>
2025-12-30 08:23:43 +0200

    feat: add Party button variant and integrate it into the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import ImageGenerationForm from "../components/ui/image-genration-form";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="">
-      <ImageGenerationForm />;
+      <ImageGenerationForm />;<Button variant="party">Party</Button>
     </main>
   );
 }
